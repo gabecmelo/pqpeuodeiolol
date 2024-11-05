50c2a6382d18a505916d14da2df6fae951c385c3
Gabriel Cabral Melo <contatogabemelo@gmail.com>
2024-11-04 21:24:41 -0300

    adiciona aplicacao completa (sim fiz tudo diretokkkk)

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import ifhLolicon from './assets/IFHLOL.png';
+import { useNavigate } from 'react-router-dom';
+import { helpers } from './helpers/dbHelpers';
+const FrasesComponent = () => {
+  const navigate = useNavigate();
+
+  const id = helpers.getRandomId();
+
+  return (
+    <div className='flex flex-col items-center justify-center h-screen gap-10' style={{ marginTop: '-20%' }}>
+      <img onClick={() => navigate(`/${id}`)} className='cursor-pointer' width={400} src={ifhLolicon} alt="Icone não permitido League of Legends" />
+      <p className='text-lg'>Clique o Ícone para receber uma opinião sincera.</p>
+    </div>
+  );
+};
+
+export default FrasesComponent;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer className="flex fixed bottom-0 left-0 p-4 w-full justify-center bg-zinc-800">
+      <p className='text-zinc-300'>
+        Opiniões reais de pessoas reais sobre um jogo que infelizmente também é real. <a className='text-yellow-300 underline cursor-pointer'>Deixe sua
+        opinião</a> ou me ajude a aliviar (ou aumentar) o meu ódio por LoL <a className='text-yellow-300 underline cursor-pointer'>me pagando um café</a>.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
